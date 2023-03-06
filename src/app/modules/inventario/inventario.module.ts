fc7db98ccded0ffe14e350ec1a1cfb57b456e5b6
MauricioRL15 <15rlmauricio@gmail.com>
2023-03-06 17:29:47 -0600

    ShareModule importado a inventario

--- src/app/modules/inventario/inventario.module.ts
+++ src/app/modules/inventario/inventario.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { InventarioRoutingModule } from './inventario-routing.module';
 import { InventarioComponent } from './pages/inventario/inventario.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
